81641d0d38b312405a3d3604ffa01ec1cfd92a74
Carlos Aaron Rivas Hernández <carlos.rivas@ironbit.com.mx>
2018-02-27 09:17:08 -0600

    Add Autorfy Unity AR Project

---
+++ ROA/Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
@@ -0,0 +1,126 @@
+/*==============================================================================
+Copyright (c) 2010-2014 Qualcomm Connected Experiences, Inc.
+All Rights Reserved.
+Confidential and Proprietary - Protected under copyright and other laws.
+==============================================================================*/
+
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEditor;
+using System.IO;
+using System;
+
+namespace Vuforia
+{
+    /// <summary>
+    /// A custom handler that implements the ITrackableEventHandler interface.
+    /// </summary>
+    public class DefaultTrackableEventHandler : MonoBehaviour,
+                                                ITrackableEventHandler
+    {
+        #region PRIVATE_MEMBER_VARIABLES
+ 
+        private TrackableBehaviour mTrackableBehaviour;
+    
+        #endregion // PRIVATE_MEMBER_VARIABLES
+
+
+		public GameObject uiManager, video, btns_AP, btns_NAP, btns_APH, btns_NAPH;
+		//public string URLvideoAguaPiedra = "http://clips.vorwaerts-gmbh.de/Vfe_html5.mp4";
+        #region UNTIY_MONOBEHAVIOUR_METHODS
+    
+        void Start()
+        {
+			btns_NAP.SetActive (false);
+			btns_NAPH.SetActive (false);
+            mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+            if (mTrackableBehaviour)
+            {
+                mTrackableBehaviour.RegisterTrackableEventHandler(this);
+            }
+
+        }
+
+        #endregion // UNTIY_MONOBEHAVIOUR_METHODS
+
+        #region PUBLIC_METHODS
+
+        /// <summary>
+        /// Implementation of the ITrackableEventHandler function called when the
+        /// tracking state changes.
+        /// </summary>
+        public void OnTrackableStateChanged(
+                                        TrackableBehaviour.Status previousStatus,
+                                        TrackableBehaviour.Status newStatus)
+        {
+            if (newStatus == TrackableBehaviour.Status.DETECTED ||
+                newStatus == TrackableBehaviour.Status.TRACKED ||
+                newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+            {
+                OnTrackingFound();
+            }
+            else
+            {
+                OnTrackingLost();
+            }
+        }
+
+        #endregion // PUBLIC_METHODS
+
+        #region PRIVATE_METHODS
+
+
+        private void OnTrackingFound()
+        {
+            Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
+            Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
+
+            // Enable rendering:
+            foreach (Renderer component in rendererComponents)
+            {
+                component.enabled = true;
+            }
+
+            // Enable colliders:
+            foreach (Collider component in colliderComponents)
+            {
+                component.enabled = true;
+            }
+			video.SetActive (true);
+			btns_AP.SetActive (true);
+			btns_APH.SetActive (true);
+			uiManager.GetComponent<UIManager> ().activated = true;
+			uiManager.GetComponent<UIManager> ().video1 = true;
+			GameObject.Find ("ARCamera").GetComponent<AudioSource>().enabled = true;
+
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found");
+        }
+
+
+        private void OnTrackingLost()
+        {
+            Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
+            Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
+
+            // Disable rendering:
+            foreach (Renderer component in rendererComponents)
+            {
+                component.enabled = false;
+            }
+
+            // Disable colliders:
+            foreach (Collider component in colliderComponents)
+            {
+                component.enabled = false;
+            }
+			video.SetActive (false);
+			btns_AP.SetActive (false);
+			uiManager.GetComponent<UIManager> ().activated = false;
+			uiManager.GetComponent<UIManager> ().video1 = false;
+			GameObject.Find ("ARCamera").GetComponent<AudioSource>().enabled = false;
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
+        }
+
+        #endregion // PRIVATE_METHODS
+    }
+}
